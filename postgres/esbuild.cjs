b04f3f1f70483652fa17682905b1c62646270f0c
ankit <ankitaabad.1@gmail.com>
2026-04-09 14:12:02 +0530

    postgres initial commit

---
+++ postgres/esbuild.cjs
@@ -0,0 +1,31 @@
+const esbuild = require("esbuild");
+const fs = require("node:fs");
+const path = require("node:path");
+
+// const FRONTEND_DIST = path.resolve("../frontend/dist"); // Vite output
+// const BACKEND_FRONTEND_DIST = path.resolve("dist-frontend");
+const DIST_DIR = path.resolve("dist");
+
+// Clean dist
+fs.rmSync(DIST_DIR, { recursive: true, force: true });
+esbuild
+  .build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    platform: "node",
+    format: "cjs",
+    minify: true,
+    target: ["node24"],
+    outfile: "dist/index.js",
+
+    external: ["pg"],
+    logLevel: "info",
+    metafile: true
+  })
+  .then((result) => {
+    console.log("Build succeeded");
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
