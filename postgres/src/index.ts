4eed29975d7bd2df84aef082e6d0788e48ba93d0
ankit <ankitaabad.1@gmail.com>
2026-04-09 14:12:02 +0530

    postgres initial commit

---
+++ postgres/src/index.ts
@@ -0,0 +1,29 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+
+import { db } from "./dbClient";
+import ksuid from "ksuid";
+const app = new Hono();
+
+app.post("/posts", async (c) => {
+  const { title, content } = await c.req.json();
+  const id = ksuid.randomSync().string;
+  console.log({ id, title, content });
+  await db.insertInto("posts").values({ id, title, content }).execute();
+  return c.json({ id, title, content });
+});
+
+app.get("/posts", async (c) => {
+  const posts = await db.selectFrom("posts").selectAll().execute();
+  return c.json(posts);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: parseInt(process.env.ZIPUP_PORT!) 
+  },
+  (info) => {
+    console.log(`Server is running on port ${info.port}`);
+  }
+);
